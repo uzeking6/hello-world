2688271ccfd3f29f0b2ce065f6986bd26ef7b08d
uzeking6 <81620569+uzeking6@users.noreply.github.com>
2021-08-20 00:50:28 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.ursulla;
+public class Main{
+    public static void main(String[] arg)
+    {
+        String name ="kom zeking paule ursulla";
+        System.out.print(name);
+
+        }
+    }
